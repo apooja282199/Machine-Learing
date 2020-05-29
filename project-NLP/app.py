b065740957cf7cecba969461161b8f8503f9d3da
apooja282199 <64535998+apooja282199@users.noreply.github.com>
2020-05-29 19:23:42 +0530

    project for intership

--- project-NLP/app.py
+++ project-NLP/app.py
@@ -5,7 +5,7 @@ from keras.preprocessing import image
 import tensorflow as tf
 global graph
 graph = tf.get_default_graph()
-from flask import Flask , request, render_template
+from flask import Flask , request, render_template,url_for
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
@@ -37,11 +37,19 @@ def page():
 
 @app.route('/predict',methods=['GET','POST'])
 def predict():
-    ms=request.form['ms']
-    if(ms=='positive'):
-        return render_template("base.html",label="it is "+str(ms))
-    
-
+    y_pred=request.form['ms']
+    if(y_pred =='true'):
+            img_url = url_for('static',filename = 'style/1.jpg')
+            topic = "Positive Tweet"
+    elif(y_pred =='false'):
+            img_url = url_for('static',filename = 'style/2.jpg')
+            topic = "Negative Tweet"
+    else:
+            img_url = url_for('static',filename = 'style/3.jpg')
+            print(img_url)
+            topic = "Neutral Tweet"
+    return render_template('base.html',ypred = topic)
+        
 if __name__ == '__main__':
     app.run(debug = True, threaded = False)
 
